a92e989efa57d06d7a1952077bb1b614210ebc89
Ridwan Bin Sarwar <38183049+ridwanbinsarwar@users.noreply.github.com>
2022-02-20 23:14:27 +0600

    removed phone number

--- components/Common/Footer.tsx
+++ components/Common/Footer.tsx
@@ -9,10 +9,6 @@ function Footer() {
                 <Link href={'mailto:ridwan.bin.sarwar@gmail.com'}>
                     <div className="text-xs tab:text-base">ridwan.bin.sarwar@gmail.com</div>
                 </Link>
-                <div className="bg-gray-300 w-1.5 h-1.5 rounded-full mx-[10px] tab:mx-[15px] my-[15px] tab:my-[0px]"></div>
-                <Link href={'tel:+8801532601363'}>
-                    <div className="text-xs tab:text-base">+8801532601363</div>
-                </Link>
             </div>
         </div>
     );
